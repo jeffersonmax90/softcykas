cb53d63f888234f61cf78c39512a0e9bfaa1cbca
David <David@DESKTOP-IADDTNI>
2018-03-26 22:56:15 +0200

    Añade la ventana del buscador.

--- src/main/java/ventanas/Buscador.java
+++ src/main/java/ventanas/Buscador.java
@@ -1,9 +1,9 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Buscador extends Cabecera_buscador {
-	private JComboBox _ordenarCB;
+public class Buscador extends Buscador_ventanas {
+	/*private JComboBox _ordenarCB;
 	public Zona_inicio_cabecera_invitado _unnamed_Zona_inicio_cabecera_invitado_;
 	public Zona_inicio_cabecera_registrado _unnamed_Zona_inicio_cabecera_registrado_;
 	public Lista_usuarios _unnamed_Lista_usuarios_;
-	public Lista_videos_buscados _unnamed_Lista_videos_buscados_;
+	public Lista_videos_buscados _unnamed_Lista_videos_buscados_;*/
 }
