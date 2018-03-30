2d4f54ea2c6f0f64f9c0aea540e7694620b12bb4
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2018-03-30 17:12:44 +0200

    Añadir ventana Recuperar contraseña

--- src/main/java/ventanas/Recuperar_contrasena.java
+++ src/main/java/ventanas/Recuperar_contrasena.java
@@ -1,9 +1,10 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Recuperar_contrasena {
-	private JLabel _emailLB;
+public class Recuperar_contrasena extends Recuperar_contrasena_ventanas {
+	/*private JLabel _emailLB;
 	private JTextField _emailTF;
 	private JButon _atrasB;
 	private JButon _enviarB;
 	public Iniciar_sesion _unnamed_Iniciar_sesion_;
+	*/
 }
