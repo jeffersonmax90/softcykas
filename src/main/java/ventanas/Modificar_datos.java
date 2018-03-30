58cfda6adcb81a64445a612f393092c7d0249c0b
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2018-03-30 18:00:24 +0200

    Añadir ventana Modificar datos

--- src/main/java/ventanas/Modificar_datos.java
+++ src/main/java/ventanas/Modificar_datos.java
@@ -1,6 +1,8 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Modificar_datos extends Anadir_datos_usuario {
-	private JLabel _modificar_datosLB;
+public class Modificar_datos extends Modificar_datos_ventanas {
+	
+	/*private JLabel _modificar_datosLB;
 	public Zona_opciones_perfil_registrado _unnamed_Zona_opciones_perfil_registrado_;
+	*/
 }
