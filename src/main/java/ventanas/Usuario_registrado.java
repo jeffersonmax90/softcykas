48a8538f976856528fd29e95a2b664950c107ff7
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2018-03-31 21:35:17 +0200

    Añade usuario registrado

--- src/main/java/ventanas/Usuario_registrado.java
+++ src/main/java/ventanas/Usuario_registrado.java
@@ -1,6 +1,17 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Usuario_registrado {
+import org.omg.CORBA.portable.IndirectionException;
+
+public class Usuario_registrado extends Usuario_invitado_ventanas {
+	/*
 	public Zona_inicio_cabecera_registrado _unnamed_Zona_inicio_cabecera_registrado_;
 	public Zona_inicio_cuerpo_registrado _unnamed_Zona_inicio_cuerpo_registrado_;
+	*/
+	Zona_inicio_cabecera_registrado zicabecerar= new Zona_inicio_cabecera_registrado();
+	Zona_inicio_cuerpo_registrado zicr= new Zona_inicio_cuerpo_registrado();
+	
+	public Usuario_registrado(){
+		inicio.addComponent(zicabecerar);
+		cuerpo.addComponent(zicr);
+	}
 }
--- src/main/java/ventanas/Zona_inicio_cabecera_registrado.java
+++ src/main/java/ventanas/Zona_inicio_cabecera_registrado.java
@@ -1,10 +1,18 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Zona_inicio_cabecera_registrado extends Cabecera_buscador {
+public class Zona_inicio_cabecera_registrado extends Zona_inicio_cabecera_registrado_ventanas {
+	/*
 	private JButon _mi_perfilB;
 	private JButon _cerrar_sesionB;
 	private Image _logo;
 	public Usuario_registrado _unnamed_Usuario_registrado_;
 	public Perfil_registrado _unnamed_Perfil_registrado_;
 	public Buscador _unnamed_Buscador_;
+	*/
+	Cabecera_buscador_ventanas zici= new Cabecera_buscador();
+	
+	public Zona_inicio_cabecera_registrado(){
+		buscador_layout.addComponent(zici);
+	}
+	
 }
