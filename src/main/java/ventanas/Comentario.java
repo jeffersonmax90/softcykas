6a554dca2e1d94062397a1be1897f72311e6101d
dvormagic <dvormagic@gmail.com>
2018-04-08 13:36:09 +0200

    Corrige errores de Ficha_invitado. Añade componente de comentario.

--- src/main/java/ventanas/Comentario.java
+++ src/main/java/ventanas/Comentario.java
@@ -1,11 +1,12 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Comentario {
-	private Image _fotoI;
-	private Label _nombre_usuarioLB;
-	private Label _comentariosLB;
-	private Button _borrar_cometarioB;
-	public Lista_comentarios_anadibles _unnamed_Lista_comentarios_anadibles_;
-	public Lista_comentarios_deshabilitables _unnamed_Lista_comentarios_deshabilitables_;
-	public Perfil_visitante _unnamed_Perfil_visitante_;
+public class Comentario extends Comentario_ventanas{
+	/*
+	 * private Image _fotoI; private Label _nombre_usuarioLB; private Label
+	 * _comentariosLB; private Button _borrar_cometarioB; public
+	 * Lista_comentarios_anadibles _unnamed_Lista_comentarios_anadibles_; public
+	 * Lista_comentarios_deshabilitables
+	 * _unnamed_Lista_comentarios_deshabilitables_; public Perfil_visitante
+	 * _unnamed_Perfil_visitante_;
+	 */
 }
--- src/main/java/ventanas/Zona_cabecera_video_invitado.java
+++ src/main/java/ventanas/Zona_cabecera_video_invitado.java
@@ -1,6 +1,6 @@
 package ventanas;
 
-public class Zona_cabecera_video_invitado {
+public class Zona_cabecera_video_invitado extends Zona_cabecera_video_invitado_ventanas {
 	/*private Button _iniciar_sesionB;
 	private Image _logoI;
 	public Ficha_invitado _unnamed_Ficha_invitado_;
