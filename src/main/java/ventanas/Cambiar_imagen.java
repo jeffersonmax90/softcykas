5a2855c91c7aa5ce3c99321b411c14f2faed73de
dvormagic <dvormagic@gmail.com>
2018-04-10 00:27:19 +0200

    Añade componente de cambiar imagen.

--- src/main/java/ventanas/Cambiar_imagen.java
+++ src/main/java/ventanas/Cambiar_imagen.java
@@ -1,11 +1,10 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Cambiar_imagen {
-	private Label _seleccionar_imagenLB;
-	private Button _seleccionar_imagenB;
-	public Zona_cabecera_administrador _unnamed_Zona_cabecera_administrador_;
-
-	public void cambiar_imagen() {
-		throw new UnsupportedOperationException();
-	}
+public class Cambiar_imagen extends Cambiar_imagen_ventanas {
+	/*
+	 * private Label _seleccionar_imagenLB; private Button _seleccionar_imagenB;
+	 * public Zona_cabecera_administrador _unnamed_Zona_cabecera_administrador_;
+	 * 
+	 * public void cambiar_imagen() { throw new UnsupportedOperationException(); }
+	 */
 }
