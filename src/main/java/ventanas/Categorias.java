4ae1f0ffd0f29928a4b96513408f08facfa79cdf
dvormagic <dvormagic@gmail.com>
2018-04-10 00:37:03 +0200

    Añade componente de categoría.

--- src/main/java/ventanas/Categorias.java
+++ src/main/java/ventanas/Categorias.java
@@ -1,16 +1,15 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Categorias {
-	private Label _nombre_categoriaLB;
-	private Button _modificar_categoriaB;
-	private Button _eliminar_categoriaB;
-	public Lista_categorias _unnamed_Lista_categorias_;
-
-	public void modificar_categoria() {
-		throw new UnsupportedOperationException();
-	}
-
-	public void eliminar_categoria() {
-		throw new UnsupportedOperationException();
-	}
+public class Categorias extends Categorias_ventanas{
+	/*
+	 * private Label _nombre_categoriaLB; private Button _modificar_categoriaB;
+	 * private Button _eliminar_categoriaB; public Lista_categorias
+	 * _unnamed_Lista_categorias_;
+	 * 
+	 * public void modificar_categoria() { throw new
+	 * UnsupportedOperationException(); }
+	 * 
+	 * public void eliminar_categoria() { throw new UnsupportedOperationException();
+	 * }
+	 */
 }
