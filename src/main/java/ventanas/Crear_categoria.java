e95d519322a5cdc7101ece5d44313e20bd5be4cf
dvormagic <dvormagic@gmail.com>
2018-04-10 23:07:01 +0200

    Añade lista de categorías y creación de categorías.

--- src/main/java/ventanas/Crear_categoria.java
+++ src/main/java/ventanas/Crear_categoria.java
@@ -1,16 +1,16 @@
-package Diagrama_de_clases;
+package ventanas;
 
-public class Crear_categoria {
-	private Label _nombre_nueva_categoriaLB;
+public class Crear_categoria extends Crear_categoria_ventanas {
+	/*private Label _nombre_nueva_categoriaLB;
 	private TextFiel _nombre_nueva_categoriaTF;
 	private Label _edad_permitidadLb;
 	private JCombox _edad_permitidaCB;
 	private Button _crear_categoriaB;
-	private Button _atrásB;
+	private Button _atrï¿½sB;
 	public Zona_opciones_administrador _unnamed_Zona_opciones_administrador_;
 	public Lista_categorias _unnamed_Lista_categorias_;
 
 	public void crear_categoria() {
 		throw new UnsupportedOperationException();
-	}
+	}*/
 }
--- src/main/java/ventanas/Lista_categorias.java
+++ src/main/java/ventanas/Lista_categorias.java
@@ -1,12 +1,9 @@
-package Diagrama_de_clases;
+package ventanas;
 
-import java.util.Vector;
-import Diagrama_de_clases.Categorias;
-
-public class Lista_categorias {
-	private Label _lista_de_categoriasLB;
-	private Label _nombre_categoriaLB;
-	private Vector _categoriasV;
-	public Crear_categoria _unnamed_Crear_categoria_;
-	public Vector<Categorias> _unnamed_Categorias_ = new Vector<Categorias>();
-}
+public class Lista_categorias extends Lista_categorias_ventanas {
+	/*
+	 * private Label _lista_de_categoriasLB; private Label _nombre_categoriaLB;
+	 * private Vector _categoriasV; public Crear_categoria
+	 * _unnamed_Crear_categoria_; public Vector<Categorias> _unnamed_Categorias_ =
+	 * new Vector<Categorias>();
+	 */}
