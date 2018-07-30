dba450005e848899d611bd2d1b0a4c8a495d1bc8
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2018-07-30 20:57:00 +0200

    Se añade videos tendencias en Admin

--- src/main/java/ventanas/Zona_inicio_cuerpo_administrador.java
+++ src/main/java/ventanas/Zona_inicio_cuerpo_administrador.java
@@ -5,4 +5,11 @@ public class Zona_inicio_cuerpo_administrador extends Zona_inicio_cuerpo_adminis
 	 * public Usuario_administrador _unnamed_Usuario_administrador_; public
 	 * Zona_inicio_comun _unnamed_Zona_inicio_comun_;
 	 */
+	
+	
+	public Zona_inicio_cuerpo_administrador(){
+		
+		vertical.removeAllComponents();
+		vertical.addComponentAsFirst(new Zona_inicio_comun());
+	}
 }
