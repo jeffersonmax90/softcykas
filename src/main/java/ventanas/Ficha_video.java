758c29f5b5fc6909608fb69fa0c3ef8dddad0110
dvormagic <dvormagic@gmail.com>
2018-07-30 23:00:52 +0200

    Cambia componente de video en la ficha por horizontalLayout. Formatea código en algunos ficheros.
    El video aun no carga, hay que encontrar el motivo por el cual esto
    sucede.

--- src/main/java/ventanas/Ficha_video.java
+++ src/main/java/ventanas/Ficha_video.java
@@ -2,98 +2,88 @@ package ventanas;
 
 import java.util.Date;
 
+import com.vaadin.server.ExternalResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Embedded;
 
 public class Ficha_video extends Ficha_video_ventanas {
-	/*private Video _stream;
-	private Button _nombre_perfil_usuarioB;
-	private Label _descripcion_videoLB;
-	private Label _etiquetaLB;
-	private Label _categoriaLB;
-	private Label _fechaLB;
-	private Label _titulo_videoLB;
-	private Label _n_me_gustaLB;
-	private Label _n_visualizacionesLB;
-	public Perfil_visitante _unnamed_Perfil_visitante_;
-
-	public void reproducir() {
-		throw new UnsupportedOperationException();
-	}*/
-	IUsuario_registrado usuR= new BD_Principal();
-	IUsuario_no_registrado usuNoR= new BD_Principal();
-	IAdministrador admin= new BD_Principal();
+	/*
+	 * private Video _stream; private Button _nombre_perfil_usuarioB; private Label
+	 * _descripcion_videoLB; private Label _etiquetaLB; private Label _categoriaLB;
+	 * private Label _fechaLB; private Label _titulo_videoLB; private Label
+	 * _n_me_gustaLB; private Label _n_visualizacionesLB; public Perfil_visitante
+	 * _unnamed_Perfil_visitante_;
+	 * 
+	 * public void reproducir() { throw new UnsupportedOperationException(); }
+	 */
+	IUsuario_registrado usuR = new BD_Principal();
+	IUsuario_no_registrado usuNoR = new BD_Principal();
+	IAdministrador admin = new BD_Principal();
 	private int idAutor;
+
 	public Ficha_video() {
 		// TODO Auto-generated constructor stub
-		
+
 		cargarFichaVideoRegistrado();
-		
-		
+
 	}
 
-	 void cargarFichaVideoRegistrado() {
-		 Video_BD v= usuR.cargarFichaVideoRegistrado(Datos_Navegante.getIdVideo());
-		 titulo_video.setValue(v.getTitulo());
-		 int visualizaciones= v.getVisualizaciones();
-		 String n= "";
-		 n=String.valueOf(visualizaciones);
-		 Date d= v.getFecha_subida();
-		 String fecha= String.valueOf(d);
-		 nombre_usuario.setCaption(v.getPropietario().getNombre());
-		 nombre_usuario.addClickListener(new Button.ClickListener() {
-				@Override
-				public void buttonClick(ClickEvent event) {
-					UI.getCurrent().getNavigator().navigateTo("perfil_registrado");
-				}
-			});
-		 
-		 
-		 numero_me_gusta.setValue(n);
-		 fecha_subida.setValue(fecha);
-		 descripcion_video.setValue(v.getDescripcion());
-		 categoria.setValue(v.getCategoria_BD().getNombre()+ " "+ v.getCategoria_BD().getEdad());
-		 etiquetas.setValue(v.getEtiqueta());
-		 idAutor=v.getPropietario().getORMID();
-		 this.nombre_usuario.addClickListener(new Button.ClickListener() {
-				@Override
-				public void buttonClick(ClickEvent event) {
-					
-				
-					
-					int idvisitante= v.getPropietario().getId();
-					//Datos_Navegante.setIdUsuario(idvisitante);
-					
-					if(Datos_Navegante.getTipoUsuario().equals("Invitado")) {
-		        		UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
-		        	}else if(Datos_Navegante.getTipoUsuario().equals("Registrado")) {
-		        		if(Datos_Navegante.getIdUsuario()==idAutor) {
-			        		UI.getCurrent().getNavigator().navigateTo("perfil_registrado");
-			        	}else {
-			        	
-			        		UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
-			        	}
-		        	}
-		        	else {
-		        		UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
-		        	}
-					
-					
-					
-					
-					
-					
+	void cargarFichaVideoRegistrado() {
+		Video_BD v = usuR.cargarFichaVideoRegistrado(Datos_Navegante.getIdVideo());
+		Embedded vid = new Embedded(null, new ExternalResource(v.getRuta()));
+		System.out.println(v.getRuta());
+		vid.setMimeType("application/x-shockwave-flash");
+		vid.setParameter("allowFullScreen", "true");
+		vid.setWidth("800px");
+		vid.setHeight("480px");
+		layoutReproductor.addComponent(vid);
+
+		titulo_video.setValue(v.getTitulo());
+		int visualizaciones = v.getVisualizaciones();
+		String n = "";
+		n = String.valueOf(visualizaciones);
+		Date d = v.getFecha_subida();
+		String fecha = String.valueOf(d);
+		nombre_usuario.setCaption(v.getPropietario().getNombre());
+		nombre_usuario.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				UI.getCurrent().getNavigator().navigateTo("perfil_registrado");
+			}
+		});
+
+		numero_me_gusta.setValue(n);
+		fecha_subida.setValue(fecha);
+		descripcion_video.setValue(v.getDescripcion());
+		categoria.setValue(v.getCategoria_BD().getNombre() + " " + v.getCategoria_BD().getEdad());
+		etiquetas.setValue(v.getEtiqueta());
+		idAutor = v.getPropietario().getORMID();
+		this.nombre_usuario.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+
+				int idvisitante = v.getPropietario().getId();
+				// Datos_Navegante.setIdUsuario(idvisitante);
+
+				if (Datos_Navegante.getTipoUsuario().equals("Invitado")) {
+					UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
+				} else if (Datos_Navegante.getTipoUsuario().equals("Registrado")) {
+					if (Datos_Navegante.getIdUsuario() == idAutor) {
+						UI.getCurrent().getNavigator().navigateTo("perfil_registrado");
+					} else {
+
+						UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
+					}
+				} else {
 					UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
 				}
-			});
-		 
-	 }
-	
-	
-	
-	
-	
-	
-	
+
+				UI.getCurrent().getNavigator().navigateTo("Perfil_visitante");
+			}
+		});
+
+	}
+
 }
--- src/main/java/ventanas/Lista_total_videos_subidos.java
+++ src/main/java/ventanas/Lista_total_videos_subidos.java
@@ -8,45 +8,41 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 
-public class Lista_total_videos_subidos extends Lista_total_videos_subidos_ventanas implements View{
+public class Lista_total_videos_subidos extends Lista_total_videos_subidos_ventanas implements View {
 	/*
 	 * private TextField _buscar_videoTF; private Button _buscar_videoB; private
 	 * Vector _videos_administradorV; public Zona_opciones_administrador
 	 * _unnamed_Zona_opciones_administrador_;
 	 */
-	
-	IAdministrador usuAdmin= new BD_Principal();
-	Video_BD video= new Video_BD();
-	
-	public Lista_total_videos_subidos(){
+
+	IAdministrador usuAdmin = new BD_Principal();
+	Video_BD video = new Video_BD();
+
+	public Lista_total_videos_subidos() {
 		cargarListaTotalVideosSubidos();
-		
+
 		buscar.addClickListener(new Button.ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
-				//buscarVideosPropios();
+				// buscarVideosPropios();
 			}
 		});
-		
-		
+
 		atras.addClickListener(new ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				// TODO Auto-generated method stub
 				UI.getCurrent().getNavigator().navigateTo("perfil_administrador");
-				
 			}
 		});
 	}
 
 	void cargarListaTotalVideosSubidos() {
-		List<Video_BD> listavideos= usuAdmin.cargarListaTotalVideosSubidos(Datos_Navegante.getIdUsuario());
+		List<Video_BD> listavideos = usuAdmin.cargarListaTotalVideosSubidos(Datos_Navegante.getIdUsuario());
 		listaVideosFL.removeAllComponents();
-		for(int i=0;i<listavideos.size();i++) {
+		for (int i = 0; i < listavideos.size(); i++) {
 			listaVideosFL.addComponent(new Video(listavideos.get(i)));
 		}
-		
 	}
-	
 }
