79eeb2fef194a2f5479e2cbfd1dfd527898a3836
dvormagic <dvormagic@gmail.com>
2018-08-18 12:17:51 +0200

    Ajusta buscador y cabecera de inicio. Añade logo a página de invitado.

--- src/main/java/ventanas/Cabecera_buscador.java
+++ src/main/java/ventanas/Cabecera_buscador.java
@@ -1,56 +1,54 @@
 package ventanas;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vaadin.server.FileResource;
+import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
 
 public class Cabecera_buscador extends Cabecera_buscador_ventanas {
-	/*private JButon _buscarB;
-	private JTextField _buscarTF;
-	private JComboBox _tituloCB;
-	private JComboBox _ordenarCB;
-	*/
-	
-	public Cabecera_buscador(){
+	public Cabecera_buscador() {
 		inicializar();
-		
+
 		buscar.addClickListener(new Button.ClickListener() {
 			@Override
-			public void buttonClick(ClickEvent event) {		
+			public void buttonClick(ClickEvent event) {
 				Datos_Navegante.setBusqueda(buscarTF.getValue());
-				if( titulo.getSelectedItem().equals("")){
+				if (titulo.getSelectedItem().equals("")) {
 					UI.getCurrent().getNavigator().navigateTo("usuario_invitado");
-				}else if (Datos_Navegante.getTipoBusqueda().equals("Elige Busqueda")){
+				} else if (Datos_Navegante.getTipoBusqueda().equals("Elige Busqueda")) {
 					UI.getCurrent().getNavigator().navigateTo("");
-			
-			}else{
-				UI.getCurrent().getNavigator().navigateTo("Buscador");
-			}
+				} else {
+					UI.getCurrent().getNavigator().navigateTo("Buscador");
+				}
 			}
 		});
-		
-		 this.titulo.addValueChangeListener(event -> {
-		      String tipo= (String) event.getValue();
-		      Datos_Navegante.setTipoBusqueda(tipo);
-	      });
-		
-		
-		
+
+		this.titulo.addValueChangeListener(event -> {
+			String tipo = (String) event.getValue();
+			Datos_Navegante.setTipoBusqueda(tipo);
+		});
+
 	}
 
-	void inicializar() {
-		List<String> lista= new ArrayList<String>();
+	void inicializar() {		
+		String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+		FileResource resource = new FileResource(new File(basepath + "src/main/resources/images/logo.png"));
+		logo = new Image("Garbitube", resource);
+		
+		System.out.println(logo.getWidth());
+		
+		List<String> lista = new ArrayList<String>();
 		lista.add("Elige Busqueda");
 		lista.add("Videos");
 		lista.add("Usuarios");
-		
+
 		titulo.setItems(lista);
 		titulo.setSelectedItem(lista.get(0));
-				
 	}
-	
-	
 }
--- src/main/java/ventanas/Zona_inicio_cabecera_invitado.java
+++ src/main/java/ventanas/Zona_inicio_cabecera_invitado.java
@@ -11,19 +11,11 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
 
 public class Zona_inicio_cabecera_invitado extends Zona_inicio_cabecera_invitado_ventanas implements View {
-	/*
-	 * private Image _logo; private JButon _iniciar_sesionB; public Usuario_invitado
-	 * _unnamed_Usuario_invitado_; public Iniciar_sesion _unnamed_Iniciar_sesion_;
-	 * public Buscador _unnamed_Buscador_;
-	 */
 	public Zona_inicio_cabecera_invitado() {
-		
-	
-		
 		iniciar_sesion.addClickListener(new Button.ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
-					UI.getCurrent().getNavigator().navigateTo("logIn");
+				UI.getCurrent().getNavigator().navigateTo("logIn");
 			}
 		});
 	}
