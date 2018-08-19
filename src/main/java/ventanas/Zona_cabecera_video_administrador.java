e0999797431f63a0042cd56efa084d2f4e0f4f58
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2018-08-19 11:51:12 +0200

    se añade logo al video administrador

--- src/main/java/ventanas/Zona_cabecera_video_administrador.java
+++ src/main/java/ventanas/Zona_cabecera_video_administrador.java
@@ -1,6 +1,7 @@
 package ventanas;
 
 import com.vaadin.event.MouseEvents.ClickListener;
+import com.vaadin.server.ExternalResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
@@ -15,6 +16,9 @@ public class Zona_cabecera_video_administrador extends Zona_cabecera_video_admin
 	
 	public Zona_cabecera_video_administrador(){
 			
+		logo.setSource(new ExternalResource(
+				"https://raw.githubusercontent.com/jeffersonmax90/softcykas/master/src/main/resources/images/logo.png"));
+		
 			logo.addClickListener(new ClickListener() {
 				@Override
 				public void click(com.vaadin.event.MouseEvents.ClickEvent event) {
